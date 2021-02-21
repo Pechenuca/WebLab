20366f0ad9d1b340774b4e179fb3f8cd2582472c
Pechenuca <maxim.epifanov8@gmail.com>
2021-02-22 01:44:41 +0300

    почти все переделал, правда, самую малость опопздал

---
+++ Lab1/clearHistory.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+session_destroy();
+
+//echo "<pre>";
+//print_r($_SESSION);
+//echo "</pre>";
+?>
+
+<html>
+<head>
+    <meta http-equiv="refresh" content="0;url=main.php">
+</head>
+</html>
